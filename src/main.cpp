4c0fafa943de6e392691e1996262c004e9ffee88
lilavati <hlaura.toro@gmail.com>
2025-02-22 11:20:28 -0400

    Updated code to spanish

--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,36 @@
 
 #ifndef STASSID
 #define STASSID ""
-#define STAPSK ""
+#define STACONTRASENA ""
 #define UBIDOTS_TOKEN ""
-#define DEVICE_NAME "dispositivoecg"
+#define NOMBRE_DISPOSITIVO "dispositivoecg"
 #define VARIABLE_LABEL "senal_ecg"
+#define SENSOR A0
 #endif
 
 const char* ssid = STASSID;
-const char* password = STAPSK;
+const char* contrasena = STACONTRASENA;
 
 Ubidots client(UBIDOTS_TOKEN);
 
-const int numSamples = 500;
-int ecgValues[numSamples]; // Array para almacenar los valores del ECG
+const int TAMANO_VENTANA = 10; 
+float lecturasEcg[TAMANO_VENTANA];
+int indiceLectura = 0;
 
-void generateECGSignal();
+float obtenerPromedio(float nuevaLectura) {
+  float suma = -lecturasEcg[indiceLectura];
+  lecturasEcg[indiceLectura] = nuevaLectura;
+  suma += nuevaLectura;
+  indiceLectura = (indiceLectura + 1) % TAMANO_VENTANA;
+  float sumaActual = 0;
+  for (int i = 0; i < TAMANO_VENTANA; i++) {
+    sumaActual += lecturasEcg[i];
+  }
+  return sumaActual / TAMANO_VENTANA;
+}
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
+  Serial.print("El mensaje arribo [");
   Serial.print(topic);
   Serial.print("] ");
   for (int i=0;i<length;i++) {
@@ -33,47 +45,22 @@ void callback(char* topic, byte* payload, unsigned int length) {
 void setup() {
 
   Serial.begin(115200);
-  // client.setDebug(true); // Pass a true or false bool value to activate debug messages
-  client.wifiConnection(STASSID, STAPSK);
+  // client.setDebug(true); // To activate debug messages
+  client.wifiConnection(STASSID, STACONTRASENA);
   client.begin(callback);
+  pinMode(SENSOR, INPUT);
 }
 
 void loop() {
 
-if(!client.connected()){
-  client.reconnect();
-  }
+  if(!client.connected()){
+    client.reconnect();
+    }
 
-  generateECGSignal();
-  for (int i = 0; i < numSamples; i++) {
-    Serial.println(ecgValues[i]); 
-    client.add(VARIABLE_LABEL, ecgValues[i]);
-    delay(5); 
-  }
-  delay(1000); // Espera antes de generar otra señal al estilo ECG
-  client.ubidotsPublish(DEVICE_NAME);
+  float ECGsinFiltro = analogRead(SENSOR); 
+  float ECGfiltrado = obtenerPromedio(ECGsinFiltro);
+  client.add(VARIABLE_LABEL, ECGfiltrado);
+  client.ubidotsPublish(NOMBRE_DISPOSITIVO);
   client.loop();
+  delay(10);
 }
-
-// Función para generar señal al estilo ECG con ruido aleatorio
-void generateECGSignal() {
-  for (int i = 0; i < numSamples; i++) {
-    float t = (float)i / numSamples * PI * 2.5f; // Parámetro temporal
-    
-    float pWave = sin(t) * exp(-t) + random(-10, 11); 
-    float qrsComplex = sin(t + PI/2) * exp(-(t - PI/4)) + random(-20, 21);
-    float tWave = sin(t + PI) * exp(-(t - PI)) + random(-15,16);
-    
-    if (i < numSamples /8) { 
-      ecgValues[i] = pWave;
-    } else if (i >= numSamples /8 && i <= numSamples*3/8){
-      ecgValues[i] = qrsComplex;
-    } else if(i >numSamples*3/8 && i<=numSamples*6/8){
-      ecgValues[i] = tWave;
-      
-     }else{
-       ecgValues[i]=random(50);
-     }
-   }
-}
-
